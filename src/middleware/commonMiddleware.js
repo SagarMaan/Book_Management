f05cc47f5a1ab181f63bc95ef187b9f436d72ac7
Sagar <sagarmaan2015@gmail.com>
2023-03-08 17:34:02 +0530

    autherization

--- src/middleware/commonMiddleware.js
+++ src/middleware/commonMiddleware.js
@@ -35,3 +35,65 @@ const isAuthenticated = async function ( req , res , next ) {
         res.status(500).send({ status: 'error', error: error.message });
     }
 }
+
+
+
+
+// =========================================== AUTHORISATION ===========================================//
+const isAuthorized = async function ( req , res , next ) {
+    try {
+        const loggedUserId = req.token.userId;
+
+        if (req.originalUrl === "/books") {
+            let userId = req.body.userId;
+        
+            if (userId && typeof userId != "string") {
+                return res.status(400).send({ status: false, message: "userId must be in string." });
+            }
+            if (!userId || !userId.trim()) {
+                return res.status(400).send({ status: false, message: "userId must be present for Authorization." });
+            }
+            userId = userId.trim();
+            if (!isValidObjectId(userId)) {
+                return res.status(400).send({ status: false, message: "Invalid user id" });
+            }
+
+            const userData = await userModel.findById(userId);
+            if (!userData) {
+                return res.status(404).send({ status: false, message: "The user id does not exist" });
+            }
+
+            if (loggedUserId != userId) {
+                return res.status(403).send({ status: false, message: "Not authorized,please provide your own user id for book creation" });
+            }
+
+            req.body.userId = userId;
+        } else {
+            let bookId = req.params.bookId;
+
+            if (!bookId) {
+                return res.status(400).send({ status: false, message: "book id is mandatory" });
+            }
+            if (!isValidObjectId(bookId)) {
+                return res.status(400).send({ status: false, message: "Invalid Book ID" });
+            }
+
+            let checkBookId = await bookModel.findById(bookId);
+            if (!checkBookId) {
+                return res.status(404).send({ status: false, message: "Data Not found with this book id, Please enter a valid book id" });
+            }
+
+            let userId = checkBookId.userId;
+            
+            if (userId != loggedUserId) {
+                return res.status(403).send({ status: false, message: "Not authorized,please provide your own book id" });
+            }
+        }
+        next();
+    } catch (err) {
+        return res.status(500).send({ status: false, message: err.message });
+    }
+}
+
+
+module.exports = { isAuthenticated, isAuthorized };
