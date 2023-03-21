50894befd1a894b7bafcd36867f2731afbef5331
Sagar <sagarmaan2015@gmail.com>
2023-03-21 18:01:48 +0530

    delete review

--- src/controller/reviewController.js
+++ src/controller/reviewController.js
@@ -176,3 +176,61 @@ const updateBookReview = async function (req, res) {
 }
 
 
+// ================================= Delete Revies of Books =====================================================//
+const deleteReviewById = async function (req, res) {
+    try {
+        let data = req.params;
+        let { bookId, reviewId } = data;
+
+        if (!ObjectId.isValid(bookId)) {
+            return res.status(400).send({ status: false, message: "Invalid Book ID." });
+        }
+
+        let checkBookId = await bookModel.findOne({ _id: bookId, isDeleted: false });
+        if (!checkBookId) {
+            return res.status(404).send({ status: false, message: "This book ID is not exist or might be deleted." });
+        }
+
+        if (!ObjectId.isValid(reviewId)) {
+            return res.status(400).send({ status: false, message: "Invalid Review ID." });
+        }
+
+        let checkReview = await reviewModel.findOne({ _id: reviewId, isDeleted: false });
+        if (!checkReview) {
+            return res.status(404).send({ status: false, message: "This Review ID is not exist or might be deleted." });
+        }
+
+        checkReview = checkReview.bookId;
+
+        if (checkReview != bookId) {
+            return res.status(400).send({ status: false, message: "Book ID not relevant to Review Id." });
+        }
+
+        await reviewModel.findOneAndUpdate(
+            { _id: reviewId, isDeleted: false },
+            { isDeleted: true }
+        );
+
+        await bookModel.findOneAndUpdate(
+            { _id: bookId },
+            { $inc: { reviews: -1 } },
+        );
+
+        return res.status(200).send({ status: true, message: "Successfully Deleted." });
+    } catch (error) {
+        return res.status(500).send({ status: false, message: error.message });
+    }
+}
+
+
+module.exports = { reviewBook, updateBookReview, deleteReviewById };
+
+
+
+
+
+
+
+
+
+
